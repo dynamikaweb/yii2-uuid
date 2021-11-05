e9b3a9967f30d75527bfb2cca40067f6db183710
RodrigoDornelles <rodrigo@dornelles.me>
2021-11-05 11:11:08 -0300

    new mask independent of other thirdy

--- src/UuidMask.php
+++ src/UuidMask.php
@@ -4,5 +4,6 @@ namespace dynamikaweb\uuid;
 
 class UuidMask extends \yii\widgets\MaskedInput
 {
-    public $mask = \Ramsey\Uuid\Validator\GenericValidator::VALID_PATTERN;
+    public $mask = ['HHHHHHHH-HHHH-HHHH-HHHH-HHHHHHHHHHHH'];
+    public $definitions = ['H' => ['validator' => '[0-9A-Fa-f]']];
 }
