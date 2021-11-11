53c93c88e40e09e31a2ed3efc0c8356101ad5c8b
RodrigoDornelles <rodrigo@dornelles.me>
2021-11-11 13:16:08 -0300

    Update UuidValidator.php

--- src/UuidValidator.php
+++ src/UuidValidator.php
@@ -2,4 +2,13 @@
 
 namespace dynamikaweb\uuid;
 
-class UuidValidator extends \macgyer\yii2uuidvalidator\UuidValidator {}
+class UuidValidator extends \yii\validators\RegularExpressionValidator
+{
+    const PATTERN_V1 = '/^[0-9A-F]{8}(-?)[0-9A-F]{4}(-?)[1][0-9A-F]{3}(-?)[89AB][0-9A-F]{3}(-?)[0-9A-F]{12}$/i';
+    const PATTERN_V2 = '/^[0-9A-F]{8}(-?)[0-9A-F]{4}(-?)[2][0-9A-F]{3}(-?)[89AB][0-9A-F]{3}(-?)[0-9A-F]{12}$/i';
+    const PATTERN_V3 = '/^[0-9A-F]{8}(-?)[0-9A-F]{4}(-?)[3][0-9A-F]{3}(-?)[89AB][0-9A-F]{3}(-?)[0-9A-F]{12}$/i';
+    const PATTERN_V4 = '/^[0-9A-F]{8}(-?)[0-9A-F]{4}(-?)[4][0-9A-F]{3}(-?)[89AB][0-9A-F]{3}(-?)[0-9A-F]{12}$/i';
+    const PATTERN_V5 = '/^[0-9A-F]{8}(-?)[0-9A-F]{4}(-?)[5][0-9A-F]{3}(-?)[89AB][0-9A-F]{3}(-?)[0-9A-F]{12}$/i';
+    
+    public $pattern = V4_PATTERN;
+}
