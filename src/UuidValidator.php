4c4f94f02e63c151cd8622fa456a9cd8a556ebc0
RodrigoDornelles <rodrigo@dornelles.me>
2021-11-11 13:28:30 -0300

    hotfix: Undefined constant

--- src/UuidValidator.php
+++ src/UuidValidator.php
@@ -10,5 +10,5 @@ class UuidValidator extends \yii\validators\RegularExpressionValidator
     const PATTERN_V4 = '/^[0-9A-F]{8}(-?)[0-9A-F]{4}(-?)[4][0-9A-F]{3}(-?)[89AB][0-9A-F]{3}(-?)[0-9A-F]{12}$/i';
     const PATTERN_V5 = '/^[0-9A-F]{8}(-?)[0-9A-F]{4}(-?)[5][0-9A-F]{3}(-?)[89AB][0-9A-F]{3}(-?)[0-9A-F]{12}$/i';
     
-    public $pattern = V4_PATTERN;
+    public $pattern = self::PATTERN_V4;
 }
